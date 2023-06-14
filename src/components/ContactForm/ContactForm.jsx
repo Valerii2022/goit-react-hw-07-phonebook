9377f1af8feab7ddfdf0a4462e95f7a750878e7d
Valerii Pometun <vmpometun@gmail.com>
2023-06-14 09:50:53 +0300

    done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,8 +37,8 @@ export const ContactForm = () => {
           type="text"
           name="name"
           pattern="[a-zA-Zа-яА-Я]+ ?[a-zA-Zа-яА-Я]+"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          placeholder="Adrian"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer"
+          placeholder="Jacob Mercer"
           onChange={handleInputChange}
           required
         />
